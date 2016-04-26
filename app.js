6416b8f227b7233bdd4f8b463cac341a29822a75
Rafael Costa <rafael@rafaelcosta.me>
2016-04-26 15:59:05 -0300

    Initial user model implementation

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 require('./setup');
 
+var User = require('./model/user');
+
 global.app.get('/info', function (req, res) {
   res.send('Low Orbit Push Cannon');
 });
 
-
+global.app.get('/:id', function (req, res) {
+  var rafaelcosta = new User('571fb4e37d03071c7ad0a0a4', function (success) {
+    if (success) {
+      res.send(JSON.stringify(rafaelcosta));
+    } else {
+      res.send('FAILED');
+    }
+  }); 
+});
--- model/user.js
+++ model/user.js
@@ -1,7 +1,30 @@
 "use strict";
-
+var ObjectId = require('mongodb').ObjectID;
 class User {
-  constructor(id) {
-    
+  constructor(id, pullCallback) {
+    this.pulled = false;
+
+    let self = this;
+    let users = global.database.collection('users');
+    users.find({_id: ObjectId(id)}).toArray(function (err, docs) {
+      if (!err) {
+        if (docs.length != 1) {
+          console.log('Could not find user with ID: ' + id);
+          pullCallback(false);
+        } else {
+          self.pulled = true;
+          let user = docs[0];
+          for (var property in user) {
+            self[property] = user[property]
+          }
+          pullCallback(true);
+        }
+      } else {
+        console.log('Could not find user with ID: ' + id);
+        pullCallback(false);
+      }
+    });   
   }
 }
+
+module.exports = User;

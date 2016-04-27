6b838df5ed6a99c386010fde1e93eb2e4512a79c
Rafael Costa <rafael@rafaelcosta.me>
2016-04-27 13:45:35 -0300

    Updated CLI

--- cli.js
+++ cli.js
@@ -3,6 +3,8 @@ var prompt = require('prompt');
 var vm = require('vm');
 var yaml = require('write-yaml');
 var uuid = require('node-uuid');
+var crypto = require('crypto');
+
 
 setup.database();
 
@@ -12,10 +14,6 @@ listenForCommand();
 
 var commands = {};
 
-commands.configure = function () {
-  console.log('Configure method called');
-}
-
 commands.seed = function () {
   console.log('STARTING SEEDING PROCESS');
   console.log('ATTENTION: THIS WILL OVERWRITE YOUR PREVIOUS SEED');
@@ -23,13 +21,32 @@ commands.seed = function () {
   console.log('WRITING...');
   yaml.sync('./config/seed.yml', {default: {seed: seed} });
   console.log('Done');
+  global.config = require('konfig')();
+  listenForCommand();
+}
+
+commands.genkey = function () {
+  console.log('ATTENTION: This will generate the key using the seed!');
+
+  var seed = global.config.seed.seed;
+  prompt.get(['identifier'], function (err, result) {
+    if (!err) {
+      var composition = result.identifier + seed;
+      var key = crypto.createHash('sha256').update(composition).digest('hex');
+      console.log('KEY: ' + key);
+      listenForCommand();
+    } else {
+      console.log('error generating key');
+      listenForCommand();
+    }
+  });
 }
 
 function listenForCommand() {
   prompt.get(['command'], function (err, result) {
     if (err) {
       console.log('Error: ' + err);
-      process.exit();
+      listenForCommand();
     } else {
       executeCommand(result.command);
     }
@@ -39,10 +56,10 @@ function listenForCommand() {
 function executeCommand(name) {
   if (!commands[name]) {
     console.log('Error, command not found');
+    listenForCommand();
   } else {
     if (!vm.isContext(commands)) vm.createContext(commands);
     vm.runInContext(name + '();', commands);
   }
-  listenForCommand();
 }
 

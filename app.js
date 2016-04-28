0c23dceec042fb576953e4dc4578438993f14db1
Rafael Costa <rafael@rafaelcosta.me>
2016-04-28 10:16:50 -0300

    Started doing tests

--- app.js
+++ app.js
@@ -6,29 +6,9 @@ setup.express();
 var User = require('./model/user');
 var UserController = require('./controllers/userController');
 
-
 global.router.addRoute('POST', '/device', UserController.create); 
 
 global.router.addRoute('GET','/info', function (req, res) {
   res.send('Low Orbit Push Cannon');
 });
 
-//global.app.get('/id', function (req, res) {
-//  var rafaelcosta = new User({id: '571fb4e37d03071c7ad0a0a4'}, function (status) {
-//    if (status == 200) {
-//      res.send(JSON.stringify(rafaelcosta));
-//    } else {
-//      res.send('FAILED');
-//    }
-//  }); 
-//});
-
-//global.app.get('/application', function (req, res) {
-//  User.usersForQuery({application: 'me.rafaelcosta.sweettweet'}, function (err, users) {
-//    if (!err) {
-//      res.send(JSON.stringify(users));
-//    } else {
-//      res.send('ERROR');
-//    }
-//  });
-//});

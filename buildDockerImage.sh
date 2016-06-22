89ec27a87e0bf9078343136fe05d52604db51d4b
Rafael Costa <rafael@rafaelcosta.me>
2016-06-22 17:02:41 -0300

    Added beta tag to docker build

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 TAG=$(git describe)
-docker build -t lopc:$TAG .
+TAG_BETA=$(git describe --abbrev=0).$(git rev-list $(git describe --abbrev=0)..HEAD --count).$(git rev-parse --abbrev-ref HEAD)
+COMMIT_COUNT_SINCE_TAG=$(git rev-list $(git describe --abbrev=0)..HEAD --count)
+
+if [ $COMMIT_COUNT_SINCE_TAG -eq 0 ]; then
+  docker build -t lopc:$TAG .
+else
+  docker build -t lopc:$TAG_BETA .
+fi

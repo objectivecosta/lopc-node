c073c4e8a29c6cce5812688468d4e94baa4e611b
Rafael Costa <rafael@rafaelcosta.me>
2016-06-23 13:54:51 -0300

    Enable API usage for any website

--- app.js
+++ app.js
@@ -6,6 +6,13 @@
 var express = require('express');
 global.app = express();
 
+// Enable API usage by any website
+global.app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var bodyParser = require('body-parser');
 
 global.app.use(bodyParser.json());

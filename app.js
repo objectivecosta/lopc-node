96a9ee0978455ccc0731bca8b40bc7a6bf131eec
Rafael Costa <rafael@rafaelcosta.me>
2016-06-24 16:30:55 -0300

    Started refactor

--- app.js
+++ app.js
@@ -3,52 +3,16 @@
 
 // Setup Webserver:
 
-var https = require('https');
-var fs = require('fs');
 var express = require('express');
+var Web = require('./web');
+
 global.app = express();
 
-// Enable API usage by any website
-global.app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-var bodyParser = require('body-parser');
-
-global.app.use(bodyParser.json());
-
-global.config = require('konfig')();
+global.config = require('konfig')(); 
 
 require('./lib/mongo')();
 
-var ObjectId = require('mongodb').ObjectId;
-
-var Router = require('./router');
-var ApplePushNotificationService = require('./lib/apns.js');
-
-global.router = new Router(global.app);
-
-var startServer = function () {
-
-  var serverOptions = {
-     key  : fs.readFileSync(global.config.ssl.certificateKeyPath),
-     cert : fs.readFileSync(global.config.ssl.certificatePath)
-  };
-
-  global.server = https.createServer(serverOptions, global.app).listen(3000, function () {
-     console.log('Started listening on port 3000');
-  });
-}
-
-setInterval(function () {
-  console.log("Restarting webserver (to reload certificates)");
-  global.server.close();
-  startServer();
-}, global.config.ssl.reloadInterval);
-
-startServer();
+Web.startup();
 
 // Routes:
 

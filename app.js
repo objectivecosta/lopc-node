b6a8223582eb4396c315b4c09c454be4f44e268c
Rafael Costa <rafael@rafaelcosta.me>
2016-06-27 10:34:32 -0300

    Changed routes: will start working on auth

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ var UserController = require('./controllers/userController');
 var PushController = require('./controllers/pushController');
 var AppController = require('./controllers/appController');
 
-global.router.addRoute('GET', '/apps', AppController.allApps);
+global.router.addRoute('GET', '/u/apps', AppController.allApps);
 
-global.router.addRoute('POST', '/push', PushController.send);
+global.router.addRoute('POST', '/a/push', PushController.send);
 
-global.router.addRoute('POST', '/device', UserController.create);
-global.router.addRoute('GET', '/device', UserController.index);
-global.router.addRoute('POST', '/device/query', UserController.search);
-global.router.addRoute('GET', '/device/:id', UserController.show);
+global.router.addRoute('POST', '/a/device', UserController.create);
+global.router.addRoute('GET', '/u/device', UserController.index);
+global.router.addRoute('POST', '/u/device/query', UserController.search);
+global.router.addRoute('GET', '/u/device/:id', UserController.show);
 
 global.router.addRoute('GET','/info', function (req, res) {
   git.getLastTag(function (err, tag) {

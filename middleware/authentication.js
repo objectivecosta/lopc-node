6714819e56a48cbedbb4e40b6b754e4e94cf4b94
Rafael Costa <rafael@rafaelcosta.me>
2016-07-06 22:44:55 -0300

    Update app auth

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -71,7 +71,31 @@ function _validateAppAuth(req, res, next) {
   if (req.path.startsWith('/device')) {
     res.status(403).json({result: 'NOK', error: 'Invalid authentication'});
   } else {
-    next();
+
+    if (!req.query.appId || !req.get('x-app-client-token')) {
+      res.status(400).json({result: 'NOK', error: 'Missing fields'});
+      return;
+    }
+
+    var appId = req.query.appId;
+    var appServerToken = req.get('x-app-server-token');
+
+    App.appsForQuery({_id : new ObjectId(appId)}, true, function (err, apps) {
+      if (err) {
+        res.status(500).json({result: 'NOK', error: 'Database error'});
+      } else {
+        if (apps.length == 0) {
+          res.status(403).json({result: 'NOK', error: 'Invalid authentication'});
+        } else {
+          var app = apps[0];
+          if (app.appServerToken == appServerToken) {
+            next();
+          } else {
+            res.status(403).json({result: 'NOK', error: 'Invalid authentication'});
+          }
+        }
+      }
+    });
   }
 }
 
